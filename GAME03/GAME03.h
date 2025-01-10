a785af9bf532027fe74600abf73c6e339ae237c0
keisaru0820@gmail.com <keisaru0820@gmail.com>
2025-01-10 11:51:34 +0900

    小田　途中経過

--- GAME03/GAME03.h
+++ GAME03/GAME03.h
@@ -12,40 +12,5 @@ namespace GAME03
         void proc();
         void destroy();
 
-        void title();
-        void play();
-        void gameclear();
-
-        void setnum();
-        void mousecol();
-        void draw();
-    private:
-
-        enum STATE { TITLE, PLAY, CLEAR };
-        STATE State = TITLE;
-
-        struct SQUARE {
-            float px;
-            float py;
-            float w;
-            float h;
-
-            int num;
-            int ansnum;
-        };
-
-
-        struct FRAME {
-            float px;
-            float py;
-            float w;
-            float h;
-        };
-
-        int num_list[9];
-
-        FRAME frame[3][3];
-        SQUARE square[9][9];
-
     };
 }
